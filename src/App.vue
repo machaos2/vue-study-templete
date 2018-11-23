a21dfcc11ccd1b5b884062f1b0a2677687287c1a
machao <2653406590@qq.com>
2018-11-23 15:04:06 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
-<!--<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>-->
-
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Welcome to VuejsCaff.com</h1>
-  </div>
+	<div id="wrap">
+		<TheHeader/>
+		<TheFooter/>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	import TheHeader from '@/components/layouts/TheHeader'
+	import TheFooter from '@/components/layouts/TheFooter'
+	export default {
+		name: 'App',
+		components: {
+			TheHeader,
+			TheFooter
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 200px;
-}
+<style lang="scss">
+	$container-large-desktop: 1200px;
+	$btn-primary-bg: #00b5ad;
+	$btn-primary-border: #00b5ad;
+	$label-primary-bg: #00b5ad;
+	$pagination-active-bg: #00b5ad;
+	$pagination-active-border: #00b5ad;
+	$pagination-color: #00b5ad;
+	$input-border-focus: #00b5ad;
+	$link-color: #12c4c5;
+	$link-hover-color: #22ddde;
+	$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+	$fa-font-path: "~font-awesome/fonts/";
+	@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+	@import "~font-awesome/scss/font-awesome";
+	@import "./styles/main";
+	@import "./styles/extra";
 </style>
