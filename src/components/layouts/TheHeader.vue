5eb3d8a11b3365c65ced9889ab5f17c3ca8064cf
machao <2653406590@qq.com>
2018-11-26 09:43:49 +0800

    11.26

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -9,10 +9,14 @@
           <span class="icon-bar"></span>
         </button>
 
-        <a href="/" class="navbar-brand">
+        <!--<a href="/" class="navbar-brand">
           <span class="title">{{ logo.title }}</span>
           <img :src="logo.src" :alt="logo.title">
-        </a>
+        </a>-->
+        <router-link to="/" class="navbar-brand">
+				  <span class="title">{{ logo.title }}</span>
+				  <img :src="logo.src" :alt="logo.title">
+				</router-link>
       </div>
 
       <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
@@ -21,14 +25,23 @@
             <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
           </li>
         </ul>
+        
+        <div class="navbar-right">
+			    <TheEntry/>
+			  </div>
+			  
       </div>
     </div>
   </div>
 </template>
 
 <script>
+	import TheEntry from '@/components/layouts/TheEntry'
 export default {
   name: 'TheHeader',
+  components: {
+    TheEntry
+  },
   data() {
     return {
       logo: {
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
+//import Vue from 'vue'
+//import Router from 'vue-router'
+//import HelloWorld from '@/components/HelloWorld'
+//
+//Vue.use(Router)
+//
+//export default new Router({
+//routes: [
+//  {
+//    path: '/',
+//    name: 'HelloWorld',
+//    component: HelloWorld
+//  }
+//]
+//})
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/auth/register',
+    name: 'Register',
+    component: () => import('@/views/auth/Register')
+  }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes
 })
+
+export default router
